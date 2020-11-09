8c10ec2321c6feb30b7ca53f256165c3a9128532
Leonardo Gomes Batista <leonardog.bat@gmail.com>
2020-11-09 16:45:23 -0300

    Add 'for', 'loop', 'times' and 'while' statements

---
+++ for.rb
@@ -0,0 +1,7 @@
+games = ['Persona 3', 'Final Fantasy XIII', 'Tekken 7', 'Persona 5']
+count = 0
+
+for game in games
+  count = count + 1
+  puts "#{count}: #{game}"
+end
---
+++ loop.rb
@@ -0,0 +1,10 @@
+count = 1
+
+loop do
+  puts count
+  # break if count == 10
+  if count == 10
+    break
+  end
+  count += 1
+end
