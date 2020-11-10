bd07ca12a05f95701fbb15e523919b9fd65e9584
Leonardo Gomes Batista <leonardog.bat@gmail.com>
2020-11-10 16:42:12 -0300

    Add simple array sum exercise

---
+++ arraysum.rb
@@ -0,0 +1,12 @@
+# Print the sum of the array's elements as a single integer.
+# Ex: [5,4,1] >> output: 10
+
+numbers = [1,3,3,7,8]
+
+count = 0
+
+for number in numbers
+  count += number
+end
+
+print count
